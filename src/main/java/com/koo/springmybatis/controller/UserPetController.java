7a6d51dc07c62223e8e58f8779ea800bf8155ae0
古才良 <gucailiang@aobi.com>
2017-08-22 14:27:17 +0800

    测试效果的控制器

---
+++ src/main/java/com/koo/springmybatis/controller/UserPetController.java
@@ -0,0 +1,29 @@
+package com.koo.springmybatis.controller;
+
+import com.koo.springmybatis.dao.UserPetService;
+import com.koo.springmybatis.entity.UserPet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by gucailiang on 2017/8/22.
+ */
+
+@RestController
+@RequestMapping("userPet")
+public class UserPetController {
+    @Autowired
+    UserPetService userPetService;
+
+    @RequestMapping(value="testSelect/{id}")
+    public Object testSelect(@PathVariable("id") int id ) {
+        UserPet select = userPetService.select(id);
+        System.out.println(select.toString());
+        return select.getName().toString();
+    }
+
+
+
+}
