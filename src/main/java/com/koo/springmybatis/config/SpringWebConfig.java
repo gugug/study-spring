527ecedf1453e9dd6e868d770ee6705cd465c801
古才良 <gucailiang@aobi.com>
2017-08-24 09:16:19 +0800

    增加视图解析freemarker的注解配置

--- src/main/java/com/koo/springmybatis/config/SpringWebConfig.java
+++ src/main/java/com/koo/springmybatis/config/SpringWebConfig.java
@@ -1,10 +1,14 @@
 package com.koo.springmybatis.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 /**
  * Created by gucailiang on 2017/8/22.
@@ -19,4 +23,17 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("*.html").addResourceLocations("/");
     }
 
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.enableContentNegotiation(new MappingJackson2JsonView());
+//        registry.jsp("/WEB-INF/views/",".jsp");
+        registry.freeMarker().cache(false);
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/WEB-INF/");
+        return configurer;
+    }
 }
