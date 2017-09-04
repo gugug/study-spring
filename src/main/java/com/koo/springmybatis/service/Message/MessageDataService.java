a705a21d77599d61a838ba489221681b5bc95760
gugug <734093894@qq.com>
2017-09-05 00:28:38 +0800

    删除mapper的数据库，改为文件

--- src/main/java/com/koo/springmybatis/service/Message/MessageDataService.java
+++ src/main/java/com/koo/springmybatis/service/Message/MessageDataService.java
@@ -1,28 +1,25 @@
 package com.koo.springmybatis.service.Message;
 
+import com.koo.springmybatis.constant.GlobalConfig;
+import com.koo.springmybatis.utils.FileHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- *
  * Created by gucailiang on 2017/8/25.
  */
 @Service
 public class MessageDataService {
 
     @Resource
-    MessageDataMapper messageDataMapper;
+    FileHelper fileHelper;
 
-
-    public MessageData sender(int temperature, int humidity) {
-        return messageDataMapper.sender(temperature, humidity);
-    }
-
-    public boolean saveInfo(int temperature, int humidity) {
-        return messageDataMapper.saveInfo(temperature,humidity);
+    public void saveInfo(int temperature, int humidity) {
+        fileHelper.doFileWrite(GlobalConfig.RECORD_DIR, temperature + " - " + humidity);
     }
 
 }
