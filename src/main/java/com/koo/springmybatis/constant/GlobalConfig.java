abeae2cba7b1f6c930f7f10584cc945aae360d02
gugug <734093894@qq.com>
2017-09-05 00:27:40 +0800

    常量的配置

--- src/main/java/com/koo/springmybatis/constant/GlobalConfig.java
+++ src/main/java/com/koo/springmybatis/constant/GlobalConfig.java
@@ -1,4 +1,6 @@
 package com.koo.springmybatis.constant;
 
 public interface GlobalConfig {
+    final static String ROOT_DIR = "D:/IdeaProjects/study-spring/";
+    final static String RECORD_DIR = ROOT_DIR + "Record/record.txt";
 }
