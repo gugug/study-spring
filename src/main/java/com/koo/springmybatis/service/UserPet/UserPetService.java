a1dacc245ba68d012afe61a77c95e76fb682866d
古才良 <gucailiang@aobi.com>
2017-09-04 14:31:23 +0800

    移动位置

---
+++ src/main/java/com/koo/springmybatis/service/UserPet/UserPetService.java
@@ -0,0 +1,20 @@
+package com.koo.springmybatis.service.UserPet;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+
+/**
+ * Created by gucailiang on 2017/8/22.
+ */
+@Service
+public class UserPetService implements Serializable {
+
+    @Resource
+    private UserPetMapper userPetMapper;
+
+    public UserPet select(int userId){
+        return userPetMapper.select(userId);
+    }
+}
---
+++ src/main/java/com/koo/springmybatis/service/UserPet/UserPetSqlProvider.java
@@ -0,0 +1,17 @@
+package com.koo.springmybatis.service.UserPet;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * Created by gucailiang on 2017/8/22.
+ */
+public class UserPetSqlProvider {
+    public String selectByUserIdAndName(@Param("name") String name, @Param("UserId") int userId) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("select * from UserPet where ");
+        sb.append("userId=#{userId}");
+        sb.append(" and ");
+        sb.append("name=#{name}");
+        return sb.toString();
+    }
+}
