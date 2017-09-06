ec047ce54310daecf01e31e6ecefcbaab04dd41c
gugug <734093894@qq.com>
2017-09-07 01:29:48 +0800

    增加读取properties的文件代码

--- src/main/java/com/koo/springmybatis/constant/GlobalConfig.java
+++ src/main/java/com/koo/springmybatis/constant/GlobalConfig.java
@@ -1,6 +1,30 @@
 package com.koo.springmybatis.constant;
 
-public interface GlobalConfig {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GlobalConfig {
     final static String ROOT_DIR = "D:/IdeaProjects/study-spring/";
-    final static String RECORD_DIR = ROOT_DIR + "Record/record.txt";
+    public static String RECORD_DIR = "Record/record.txt";
+
+    public GlobalConfig() {
+        RECORD_DIR = getValue("RECORD.PATH");
+    }
+
+    private static Properties prop = new Properties();
+
+    static {
+        ClassLoader loader = GlobalConfig.class.getClassLoader();
+        InputStream in = loader.getResourceAsStream("my-config.properties");
+        try {
+            prop.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getValue(String key) {
+        return prop.getProperty(key);
+    }
 }
