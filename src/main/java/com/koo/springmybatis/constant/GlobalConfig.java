90901e57f8e95118b094e2401cc056c0e47c5c8e
gugug <734093894@qq.com>
2017-10-12 23:09:38 +0800

    Property读取xml的profile属性

--- src/main/java/com/koo/springmybatis/constant/GlobalConfig.java
+++ src/main/java/com/koo/springmybatis/constant/GlobalConfig.java
@@ -1,15 +1,23 @@
 package com.koo.springmybatis.constant;
 
+import com.oracle.webservices.internal.api.message.PropertySet.Property;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class GlobalConfig {
-    final static String ROOT_DIR = "D:/IdeaProjects/study-spring/";
-    public static String RECORD_DIR = "Record/record.txt";
+
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalConfig.class);
+
+    @Property("HESSIAN.URL.HELLOSERVICE")
+    public static String HESSIAN_URL_HELLOSERVICE = "http://127.0.0.1:8080/hessianHelloService";
+    @Property("RECORD.PATH")
+    public static String RECORD_DIR = "http://127.0.0.1:8080/hessianHelloService";
 
     public GlobalConfig() {
-        RECORD_DIR = getValue("RECORD.PATH");
     }
 
     private static Properties prop = new Properties();
@@ -20,7 +28,8 @@ public class GlobalConfig {
         try {
             prop.load(in);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("my-config.properties 文件找不到");
+            LOG.error(e.getMessage(),e);
         }
     }
 
