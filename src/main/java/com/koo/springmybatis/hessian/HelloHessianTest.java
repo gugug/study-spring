327bf25e4983e1470045843d6b294169efbd306b
gugug <734093894@qq.com>
2017-10-12 22:48:18 +0800

    hessian客户端测试类

---
+++ src/main/java/com/koo/springmybatis/hessian/HelloHessianTest.java
@@ -0,0 +1,13 @@
+package com.koo.springmybatis.hessian;
+
+public class HelloHessianTest {
+    
+    public static void main(String[] args) {
+        HelloHessianConsumer helloHessianConsumer = new HelloHessianConsumer();
+        IHelloService helloService = helloHessianConsumer.getIHelloService();
+        System.out.println(helloService);
+        String helloHessian = helloService.getHelloName("hello hessian");
+        System.out.println(helloHessian);
+
+    }
+}
