d0cee01b9e41c7fd7767f4a5ccb4cce58a86878f
gugug <734093894@qq.com>
2017-10-12 22:46:57 +0800

    提供hessian接口

---
+++ src/main/java/com/koo/springmybatis/hessian/HelloHessianProvider.java
@@ -0,0 +1,22 @@
+package com.koo.springmybatis.hessian;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.caucho.HessianServiceExporter;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class HelloHessianProvider {
+    @Resource
+    HelloService helloService;
+
+    @Bean(name = "/hessianHelloService")
+    public HessianServiceExporter hessianTestWxQrService() {
+        System.out.println("hessianHelloService......");
+        HessianServiceExporter hessianServiceExporter = new HessianServiceExporter();
+        hessianServiceExporter.setService(helloService);
+        hessianServiceExporter.setServiceInterface(IHelloService.class);
+        return hessianServiceExporter;
+    }
+}
