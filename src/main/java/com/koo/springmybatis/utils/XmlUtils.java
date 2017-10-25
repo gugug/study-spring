208fdc0f9da85cdc6fef3aaae20e36c2bf88421a
古才良 <gucailiang@aobi.com>
2017-10-25 15:26:16 +0800

    map转化为xml的格式和xml反转为map

--- src/main/java/com/koo/springmybatis/utils/XmlUtils.java
+++ src/main/java/com/koo/springmybatis/utils/XmlUtils.java
@@ -2,7 +2,14 @@ package com.koo.springmybatis.utils;
 
 import com.koo.springmybatis.service.UserPet.UserPet;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,9 +26,76 @@ public class XmlUtils {
         return xStream.toXML(userPet);
     }
 
-    public static String mapToXml(Map<String,String> map){
+    /**
+     * <root>
+     * <entry>
+     * <string>k1</string>
+     * <string>v1</string>
+     * </entry>
+     * <entry>
+     * <string>k2</string>
+     * <string>v2</string>
+     * </entry>
+     * </root>
+     *
+     * @param map
+     * @return
+     */
+    public static String mapToXml(Map<String, String> map) {
         xStream.alias("root", Map.class);
         return xStream.toXML(map);
+    }
+
+    /**
+     * <root>
+     * <k1>v1</k1>
+     * <k2>v2</k2>
+     * </root>
+     *
+     * @param map
+     * @return
+     */
+    public static String mapToXmlWithoutEntry(Map<String, String> map) {
+        xStream.registerConverter(new MapEntryConverter());
+        xStream.alias("root", java.util.Map.class);
+        return xStream.toXML(map);
+    }
+
+    public static Map xmlToMap(String xml) {
+        return (Map<String, String>) xStream.fromXML(xml);
+    }
+
+    public static class MapEntryConverter implements Converter {
+        public boolean canConvert(Class clazz) {
+            return AbstractMap.class.isAssignableFrom(clazz);
+        }
+
+        public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) {
+            AbstractMap map = (AbstractMap) value;
+            for (Object obj : map.entrySet()) {
+                Map.Entry entry = (Map.Entry) obj;
+                writer.startNode(entry.getKey().toString());
+                Object val = entry.getValue();
+                if (null != val) {
+                    writer.setValue(val.toString());
+                }
+                writer.endNode();
+            }
+
+        }
+
+        public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+            Map<String, String> map = new HashMap<>();
+            while (reader.hasMoreChildren()) {
+                reader.moveDown();
+                String key = reader.getNodeName(); // nodeName aka element's name
+                String value = reader.getValue();
+                map.put(key, value);
+                reader.moveUp();
+            }
+            return map;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -33,9 +107,17 @@ public class XmlUtils {
         System.out.println(userPetToXml);
 
         HashMap<String, String> stringStringHashMap = new HashMap<>();
-        stringStringHashMap.put("k1","v1");
-        stringStringHashMap.put("k2","v2");
+        stringStringHashMap.put("k1", "v1");
+        stringStringHashMap.put("k2", "v2");
         String mapToXml = XmlUtils.mapToXml(stringStringHashMap);
         System.out.println(mapToXml);
+
+        String mapToXmlWithoutEntry = XmlUtils.mapToXmlWithoutEntry(stringStringHashMap);
+        System.out.println(mapToXmlWithoutEntry);
+
+        Map map = xmlToMap(mapToXmlWithoutEntry);
+        assert map.get("k1").equals("v1");
+
     }
+
 }
