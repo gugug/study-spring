db77a97bf9a9d8ac960957b6ef2373b7f422e7b4
gugug <734093894@qq.com>
2017-11-09 21:17:49 +0800

    配置类 开启组件扫描 ComponentScan

--- src/main/java/com/koo/springmybatis/config/SpringRootConfig.java
+++ src/main/java/com/koo/springmybatis/config/SpringRootConfig.java
@@ -6,11 +6,13 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
+ * 组件扫描开启ComponentScan，会扫描@component注解的组件类
+ * 如果没有其他配置的话，会扫描和配置类相同的包以及这个包下的所有子包，查找带有@component注解的类，然后会在spring中自动创建一个bean
  * Created by gucailiang on 2017/8/22.
  */
 @Configuration
 @ComponentScan("com.koo.springmybatis")
-@EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableTransactionManagement
 @Import(DaoConfig.class)
 public class SpringRootConfig {
