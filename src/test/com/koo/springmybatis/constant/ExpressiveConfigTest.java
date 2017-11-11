443d7e3afc10fc966bc88e1827fdea171d14cb51
gugug <734093894@qq.com>
2017-11-11 16:21:54 +0800

    测试用例上面加@WebAppConfiguration注解，否則會報加載出錯異常

---
+++ src/test/com/koo/springmybatis/constant/ExpressiveConfigTest.java
@@ -0,0 +1,29 @@
+package com.koo.springmybatis.constant;
+
+import com.koo.springmybatis.config.SpringRootConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gu on 2017/11/11.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = SpringRootConfig.class)
+public class ExpressiveConfigTest {
+
+    @Autowired
+    ExpressiveConfig expressiveConfig;
+
+    @Test
+    public void testProperties() throws Exception {
+        expressiveConfig.testProperties();
+    }
+
+}
